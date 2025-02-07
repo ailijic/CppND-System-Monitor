4d3dfbfc4e96a41375b4b52dcafd5fb812882fba
Andrew N. Ilijic <ailijic@gmail.com>
2025-02-07 13:51:22 +0000

    Give parser the ability to output PID cmdline

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -253,10 +253,15 @@ int LinuxParser::RunningProcesses() {
   return ret;
 }
 
-// TODO: Read and return the command associated with a process
-// REMOVE: [[maybe_unused]] once you define the function
-string LinuxParser::Command(int pid[[maybe_unused]]) {
-  return string();
+/// Read and return the command associated with a process
+string LinuxParser::Command(int pid) {
+  string line;
+  std::ifstream stream(kProcDirectory + to_string(pid) + kCmdlineFilename);
+  if (stream.is_open() && std::getline(stream, line)) {
+    return line;
+  } else {
+    return string();
+  }
 }
 
 // TODO: Read and return the memory used by a process
