556147a6ecdc9fa54e7bff442a539cdd62dea0fb
Andrew N. Ilijic <ailijic@gmail.com>
2025-02-07 15:13:53 +0000

    Give parser ability to return UID of PID

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -285,9 +285,22 @@ string LinuxParser::Ram(int pid) {
 }
 
 // TODO: Read and return the user ID associated with a process
-// REMOVE: [[maybe_unused]] once you define the function
-string LinuxParser::Uid(int pid[[maybe_unused]]) {
-  return string();
+string LinuxParser::Uid(int pid) {
+  string line;
+  string key;
+  string value;
+  std::ifstream filestream(kProcDirectory + to_string(pid) + kStatFilename);
+  if (filestream.is_open()) {
+    while (std::getline(filestream, line)) {
+      std::istringstream linestream(line);
+      while (linestream >> key >> value) {
+        if (key == "Uid:") {
+          return value;
+        }
+      }
+    }
+  }
+  return value;
 }
 
 // TODO: Read and return the user associated with a process
